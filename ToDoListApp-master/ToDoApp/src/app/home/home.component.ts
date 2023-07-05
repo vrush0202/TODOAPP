3770bb4151052544ae06bd9f9eadc1d31315642c
Vushali <vrushali2220@gmail.com>
2023-07-05 21:51:48 +0530

    Done

---
+++ ToDoListApp-master/ToDoApp/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private jwtHelper: JwtHelperService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  isUserAuthenticated() {
+    const token: any = localStorage.getItem("jwt");
+
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logOut() {
+    localStorage.removeItem("jwt");
+  }
+
+}
